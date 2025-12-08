cffc5bdd6c300b4946405eb9e29a61226d0caf4f
Oussama <odahri06@gmail.com>
2025-12-08 10:41:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Glaces',
+  webDir: 'www'
+};
+
+export default config;
